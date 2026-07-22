08ab8ff79ed4efcc42fe6a71dc4b6b34c758dc16
Samuel Curran <samuel.curran@fms-ea.com>
2026-07-22 15:09:24 +0200

    typage dans la methode init du basket

--- command.py
+++ command.py
@@ -3,5 +3,5 @@ from fruit_and_vegetable import Fruit_And_Vegetable
 class Command:
     basket: list[Fruit_And_Vegetable] 
     
-    def __init__(self, basket = []):
+    def __init__(self, basket: list[Fruit_And_Vegetable] = []):
         self.basket = basket
