3e25b6cbfff37ef1594965dfaa2fe97faa0a8ffe
Naka Rafilipomanana <naka.rafilipomanana@fms-ea.com>
2026-07-22 15:14:57 +0200

    Add class Fruitvegetable, add definition of the fruitvegetable

---
+++ fruitvegetable.py
@@ -0,0 +1,7 @@
+class Fruitvegetable:
+    def __init__(self, namefruitvegetable, quantity, price, is_unit): #definition of the fruitvegetable
+        self.name = namefruitvegetable #name of the fruitvegetable
+        self.quantity = quantity #quantity - number of units / Number of Kilogram
+        self.price = price #price of the vegetable per unit, Example: Kilogram, unit
+        self.is_unit = is_unit # check if it is unit
+
