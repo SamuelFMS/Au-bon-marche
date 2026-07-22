9b90f3366ddf31c4d9d5cf6ba713ec725bc31b9b
Samuel Curran <samuel.curran@fms-ea.com>
2026-07-22 15:08:07 +0200

    Methode init client ajout de tous les champs

--- client.py
+++ client.py
@@ -7,7 +7,8 @@ class Client:
     list_fruit_vegetable: list[Fruit_And_Vegetable]
     my_basket : Command 
     
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name: str, last_name: str, list_fruit_vegetable:list[Fruit_And_Vegetable]=[], my_basket:Command=Command()):
         self.first_name = first_name
         self.last_name = last_name
-        
+        self.list_fruit_vegetable = list_fruit_vegetable
+        self.my_basket = my_basket
