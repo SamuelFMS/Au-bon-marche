165c8a602f79277b03e7233b1ea3cf9ff620a5e2
Samuel Curran <samuel.curran@fms-ea.com>
2026-07-22 14:51:47 +0200

    Creation de la methode init pour commande

--- command.py
+++ command.py
@@ -1,3 +1,7 @@
 from fruit_and_vegetable import Fruit_And_Vegetable
+
 class Command:
     basket: list[Fruit_And_Vegetable] 
+    
+    def __init__(self, basket = []):
+        self.basket = basket
