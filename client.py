6274d9403fb35c51b91edc3cbd1e9322e3ee7daa
Samuel Curran <samuel.curran@fms-ea.com>
2026-07-22 15:29:26 +0200

    fruit vegetable import in client

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
-from fruit_and_vegetable import Fruit_And_Vegetable
+from fruitvegetable import Fruitvegetable
 from command import Command
 
 class Client:
     first_name: str
     last_name: str
-    list_fruit_vegetable: list[Fruit_And_Vegetable]
+    list_fruit_vegetable: list[Fruitvegetable]
     my_basket : Command 
     
-    def __init__(self, first_name: str, last_name: str, list_fruit_vegetable:list[Fruit_And_Vegetable]=[], my_basket:Command=Command()):
+    def __init__(self, first_name: str, last_name: str, list_fruit_vegetable:list[Fruitvegetable]=[], my_basket:Command=Command()):
         self.first_name = first_name
         self.last_name = last_name
         self.list_fruit_vegetable = list_fruit_vegetable
