05c1fd917fadad5c4ebd4826ce201e9140cc0189
Samuel Curran <samuel.curran@fms-ea.com>
2026-07-22 15:10:01 +0200

    ajout de la methode __str__ pour command

--- command.py
+++ command.py
@@ -5,3 +5,10 @@ class Command:
     
     def __init__(self, basket: list[Fruit_And_Vegetable] = []):
         self.basket = basket
+        
+    def __str__(self) -> str:
+        if len(self.basket) == 0:
+            return "Mon panier est vide"
+        print("Panier : ")
+        for b in self.basket:
+            print(b)
