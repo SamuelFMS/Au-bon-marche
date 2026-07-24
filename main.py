f86a95463b8fd0347a562087a799ee197f54972a
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-24 14:04:30 +0200

    fix: Modify content in main method.

--- main.py
+++ main.py
@@ -23,19 +23,23 @@ def main():
     6) Display all customers and the shop’s daily till balance
     """
     shop: Shop = Shop()
+
     while True:
-        # Création d'un utilisateur
-        new_client: Client = Client()
-        new_client.register_user()
-        shop = new_client.add_to_basket(shop)
+        # Create a client
+        customer: Client = Client()
+        customer.register_user()
 
-        while True:
-            if new_client.end_command():
+        # Create command to a new client
+        shop = customer.select_fruit_and_vegetable_to_add_to_basket(shop)
+
+        while not customer.end_command():
+            if shop.is_fruits_vegetables_out_of_stock():
+                print("Notre boutique a été dévalisé. Fin de la commande.")
                 break
-            shop = new_client.add_to_basket(shop)
+            shop = customer.select_fruit_and_vegetable_to_add_to_basket(shop)
 
-        print(new_client)
-        if ask_yes_or_no("Voulez vous finir la journée [y/yes/n/no]: "):
+        print(customer)
+        if shop.is_fruits_vegetables_out_of_stock() or ask_yes_or_no("Voulez-vous finir la journée [y/yes/n/no]: "):
             # Afficher le récapitulatif de la journée
             print("-" * 60)
             Client.afficher_liste_clients()
