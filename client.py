cc0220345b54d6d0dbec984c54df780d571ca2f4
Naka Rafilipomanana <naka.rafilipomanana@fms-ea.com>
2026-07-24 10:24:15 +0200

    Add comment to each method, modify sass variable to quantity.

--- client.py
+++ client.py
@@ -29,28 +29,45 @@ class Client:  # Create a Client class.
 
     @classmethod
     def afficher_liste_clients(cls):
+        """
+        Display information for each client
+        :param list client cls: is a list of clients
+        """
         print("Liste des clients\nPrénom, NOM")
         for client in cls.clients:
             print(f"{client.first_name.capitalize()}, {client.last_name.upper()}")
 
     @classmethod
     def get_price_total(cls) -> float:
+        """
+        Calculation of the total price of the basket for each client
+        :param List client cls: is a list of clients
+        :return: total price for each client
+        """
         total = 0
         for client in cls.clients:
             total += client.my_basket.price_of_basket()
         return round(total, 2)
 
     def register_user(self):
+        """
+        Save client information
+        """
         self.first_name = input("Veuillez entrez le prenom: ")
         self.last_name = input("Veuillez entrez le nom: ")
 
     def add_to_basket(self, shop: Shop) -> Shop:
+        """
+        Add a fruit or a vegetable to the basket
+        :param shop: a list of products that includes fruits and vegetables
+        :return: a shop object
+        """
         print(shop)
         while True:
-            sass = input("Veuillez entrez le nom de l'article souhaitez: ")
+            name_article = input("Veuillez entrez le nom de l'article souhaitez: ")
 
-            if sass in shop.get_list_name():
-                index_fruit_vegetable: int = shop.get_list_name().index(sass)
+            if name_article in shop.get_list_name():
+                index_fruit_vegetable: int = shop.get_list_name().index(name_article)
                 res: FruitVegetable = shop.product_list[index_fruit_vegetable]
 
                 if not res.is_out_of_stock():
@@ -60,13 +77,12 @@ class Client:  # Create a Client class.
             print("Nous avons pas réussis a trouver l'article souhaitez.")
 
         quantity_in_shop = res.quantity
-        quantity: int
 
         while True:
-            sass = input(f"Veuillez saisir la quantité({"unit" if res.is_unit else "g"}): ")
+            quantity = input(f"Veuillez saisir la quantité({"unit" if res.is_unit else "g"}): ")
 
-            if sass.isdigit():
-                quantity = int(sass)
+            if quantity.isdigit():
+                quantity = int(quantity)
                 if 0 < quantity <= res.quantity:
                     res.quantity = quantity
                     break
@@ -81,6 +97,9 @@ class Client:  # Create a Client class.
 
     @staticmethod
     def end_command() -> bool:
+        """
+        :return: boolean that check if the client has finished the command
+        """
         while True:
             input_user = input("Est-ce que vous continuez votre commande ? [Y/n]")
             if input_user.lower() == 'y' or input_user == '':
